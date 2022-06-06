5514711c8cf3216970787127850082acac6ade28
Mahnoor <mahnoorali57se@gmail.com>
2022-06-07 01:50:34 +0500

    refactored code and fixed a bug

--- app/src/main/java/com/mano/learnabc/QuizModule.java
+++ app/src/main/java/com/mano/learnabc/QuizModule.java
@@ -12,11 +12,8 @@ import java.util.Random;
 public class QuizModule extends AppCompatActivity implements View.OnClickListener {
 
     //5 buttons that serve as  options
-    ImageButton btn1;
-    ImageButton btn2;
-    ImageButton btn3;
-    ImageButton btn4;
-    ImageButton btn5;
+    ImageButton[] optionBtn;
+    int[] images;
     //resources of each button's image
     int btn1Resource;
     int btn2Resource;
@@ -31,26 +28,10 @@ public class QuizModule extends AppCompatActivity implements View.OnClickListene
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_module);
-
+        setOptions(); //setup mcq options
         intent = getIntent();
         alphabet = findViewById(R.id.alphabet);
         alphabet.setText(intent.getStringExtra("alphabet"));
-
-        //array of images corresponding to each alphabet
-        int[] images = {R.drawable.apple, R.drawable.ball,R.drawable.cat,R.drawable.duck, R.drawable.egg,R.drawable.fish,R.drawable.goat,R.drawable.house, R.drawable.ink,R.drawable.jam,R.drawable.kite,R.drawable.lion, R.drawable.milk,R.drawable.nest,R.drawable.orange,R.drawable.pen, R.drawable.queen,R.drawable.rat, R.drawable.sun,R.drawable.tree,R.drawable.umbrella,R.drawable.van, R.drawable.web,R.drawable.xylo,R.drawable.yoyo,R.drawable.zip };
-        //get id of all options
-        btn1=findViewById(R.id.option1);
-        btn2=findViewById(R.id.option2);
-        btn3=findViewById(R.id.option3);
-        btn4=findViewById(R.id.option4);
-        btn5=findViewById(R.id.option5);
-        result = findViewById(R.id.result);
-
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
 
        //generate random numbers in range of 26
         Random rand = new Random();
@@ -61,17 +42,34 @@ public class QuizModule extends AppCompatActivity implements View.OnClickListene
         btn4Resource = rand.nextInt(images.length);
         btn5Resource = rand.nextInt(images.length);
         // put random images as option at all 5 buttons
-        btn1.setImageResource(images[btn1Resource]);
-        btn2.setImageResource(images[btn2Resource]);
-        btn3.setImageResource(images[btn3Resource]);
-        btn4.setImageResource(images[btn4Resource]);
-        btn5.setImageResource(images[btn5Resource]);
+        optionBtn[0].setImageResource(images[btn1Resource]);
+        optionBtn[1].setImageResource(images[btn2Resource]);
+        optionBtn[2].setImageResource(images[btn3Resource]);
+        optionBtn[3].setImageResource(images[btn4Resource]);
+        optionBtn[4].setImageResource(images[btn5Resource]);
 
-        //TODO: add all landscape views
+
         //TODO: add 2 fnctns to avoid data loss
 
     }
 
+    public void setOptions(){
+        //array of images corresponding to each alphabet
+        images = new int[]{R.drawable.apple, R.drawable.ball, R.drawable.cat, R.drawable.duck, R.drawable.egg, R.drawable.fish, R.drawable.goat, R.drawable.house, R.drawable.ink, R.drawable.jam, R.drawable.kite, R.drawable.lion, R.drawable.milk, R.drawable.nest, R.drawable.orange, R.drawable.pen, R.drawable.queen, R.drawable.rat, R.drawable.sun, R.drawable.tree, R.drawable.umbrella, R.drawable.van, R.drawable.web, R.drawable.xylo, R.drawable.yoyo, R.drawable.zip};
+        //get id of all options
+        optionBtn[0]=findViewById(R.id.option1);
+        optionBtn[1]=findViewById(R.id.option2);
+        optionBtn[2]=findViewById(R.id.option3);
+        optionBtn[3]=findViewById(R.id.option4);
+        optionBtn[4]=findViewById(R.id.option5);
+        result = findViewById(R.id.result);
+
+        optionBtn[0].setOnClickListener(this);
+        optionBtn[1].setOnClickListener(this);
+        optionBtn[2].setOnClickListener(this);
+        optionBtn[3].setOnClickListener(this);
+        optionBtn[4].setOnClickListener(this);
+    }
 
     public void checkCorrectness(int imgNo, char letter) {
         int ascii = letter;
@@ -84,8 +82,7 @@ public class QuizModule extends AppCompatActivity implements View.OnClickListene
     }
 
     @Override
-    public void onClick(View view)
-    {
+    public void onClick(View view) {
         switch (view.getId()) {
             case R.id.option1:
                 checkCorrectness(btn1Resource, (alphabet.getText().toString()).charAt(0));
@@ -103,6 +100,8 @@ public class QuizModule extends AppCompatActivity implements View.OnClickListene
                 checkCorrectness(btn5Resource, (alphabet.getText().toString()).charAt(0));
                 break;
         }
+
+        /*
         // whatever case was executed, always move to next activity when a button is clicked after waiting for 2 sec
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -111,8 +110,9 @@ public class QuizModule extends AppCompatActivity implements View.OnClickListene
                 startActivity(intent);
             }
         }, 2000);   //2 seconds
-    }
+         */
 
+    }
 }
 
 
