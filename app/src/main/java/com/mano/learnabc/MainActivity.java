06949a0992fae4567d77246f3c400a2762111838
Mahnoor <mahnoorali57se@gmail.com>
2022-10-01 20:29:16 +0500

    updated

--- app/src/main/java/com/mano/learnabc/MainActivity.java
+++ app/src/main/java/com/mano/learnabc/MainActivity.java
@@ -14,18 +14,18 @@ Intent intent;
 ImageButton learn;
 ImageButton quiz;
 Button summary;
-Button repo;
+// Button repo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         learn=findViewById(R.id.learn);
         quiz = findViewById(R.id.quiz);
-        repo = findViewById(R.id.repo);
+    //    repo = findViewById(R.id.repo);
         summary = findViewById(R.id.Summary);
 
         summary.setOnClickListener(this);
-        repo.setOnClickListener(this);
+       // repo.setOnClickListener(this);
         learn.setOnClickListener(this);
         quiz.setOnClickListener(this);
     }
@@ -43,10 +43,12 @@ Button repo;
                 intent = new Intent(MainActivity.this, QuizModule.class );
                 startActivity(intent);
                 break;
+                /*
             case R.id.repo:
                 Uri webpage = Uri.parse("https://github.com/Mahnoor786-dev/Kids-ABC-learner");
                 intent = new Intent(Intent.ACTION_VIEW, webpage);
                 startActivity(intent);
+            */
             case R.id.Summary:
                 intent = new Intent(MainActivity.this, Summary.class );
                 startActivity(intent);
